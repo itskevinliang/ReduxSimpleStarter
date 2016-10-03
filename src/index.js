371ad6236718f81b2fab648afa7036470ca66edc
Kevin Liang <kev.liang@gmail.com>
2016-10-02 21:05:13 -0700

    Refactored App component to class component from functional component. Fetching data from YouTube.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCjx_HQ0wectVvsThbzWyyeL71iOt6H8lU';
+
 
 // Create a new component. This componenet should produce some HTML.
 
-const App = () => {
-  return (
-    <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+      <div>
       <SearchBar />
-    </div>
-  );
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
