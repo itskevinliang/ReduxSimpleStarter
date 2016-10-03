909e7d06f70358b3aca47390754fa0eb814e8826
Kevin Liang <kev.liang@gmail.com>
2016-10-02 20:10:41 -0700

    Added onChange handler for search_bar.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react';
 // Class component
 
 class SearchBar extends Component {
+  // Constructor function is called automatically whenever a new instance is created. Initialize variables, state, etc...
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
   render() {
-    return <input />;
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    );
+
   }
 }
 
